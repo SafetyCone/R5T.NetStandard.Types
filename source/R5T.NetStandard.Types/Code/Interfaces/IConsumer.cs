78b6aaf6cd783fe316057d9b46ba13edbda48803
MinexAutomation <minex.automation@gmail.com>
2019-04-30 22:20:19 -0400

    Added IProvider and IConsumer functionality.

---
+++ source/R5T.NetStandard.Types/Code/Interfaces/IConsumer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    /// <summary>
+    /// Consumes instances of type <typeparamref name="T"/> for processing.
+    /// Basically, an <see cref="Action{T}"/> as a type, not a delegate.
+    /// For functions see <see cref="IProvider{T}"/>.
+    /// </summary>
+    public interface IConsumer<in T>
+    {
+        void Consume(T value);
+    }
+
+    /// <summary>
+    /// Consumes paired instances of types <typeparamref name="T1"/> and <typeparamref name="T2"/> for processing.
+    /// Basically, an <see cref="Action{T1, T2}"/> as a type, not a delegate.
+    /// For functions see <see cref="IProvider{T}"/>.
+    /// </summary>
+    public interface IConsumer<in T1, in T2>
+    {
+        void Consume(T1 value1, T2 value2);
+    }
+
+    /// <summary>
+    /// Consumes triplet instances of types <typeparamref name="T1"/>, <typeparamref name="T2"/>, and <typeparamref name="T3"/> for processing.
+    /// Basically, an <see cref="Action{T1, T2, T3}"/> as a type, not a delegate.
+    /// For functions see <see cref="IProvider{T}"/>.
+    /// </summary>
+    public interface IConsumer<in T1, in T2, in T3>
+    {
+        void Consume(T1 value1, T2 value2, T3 value3);
+    }
+}
---
+++ source/R5T.NetStandard.Types/Code/Interfaces/IProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    /// <summary>
+    /// Provides an instance of a specified type.
+    /// Basically, a <see cref="Func{TResult}"/> as a type, not a delegate.
+    /// For actions see <see cref="IConsumer{T}"/>.
+    /// </summary>
+    public interface IProvider<T>
+    {
+        T Provide();
+    }
+
+    /// <summary>
+    /// Provides an instance of a specified output type, given an instance of a specified input type.
+    /// Basically, a <see cref="Func{T, TResult}"/> as a type, not a delegate.
+    /// For actions see <see cref="IConsumer{T}"/>.
+    /// </summary>
+    public interface IProvider<TOut, in TIn>
+    {
+        TOut Provide(TIn @in);
+    }
+
+    /// <summary>
+    /// Provides an instance of a specified output type, given an instance of a specified input type.
+    /// Basically, a <see cref="Func{T1, T2, TResult}"/> as a type, not a delegate.
+    /// For actions see <see cref="IConsumer{T}"/>.
+    /// </summary>
+    public interface IProvider<out TOut, in TIn1, in TIn2>
+    {
+        TOut Provide(TIn1 in1, TIn2 in2);
+    }
+}
