89e6e787e0053ed5d2ff91628e3c017cb76ac8e4
MinexAutomation <minex.automation@gmail.com>
2019-04-30 22:04:23 -0400

    Added INamed interface.

---
+++ source/R5T.NetStandard.Types/Code/Interfaces/INamed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    /// <summary>
+    /// Interface for types whose instances are named (have a name).
+    /// </summary>
+    public interface INamed
+    {
+        string Name { get; }
+    }
+}
