443fc5ad6f7344fb220feeb8eedaab7540349809
MinexAutomation <minex.automation@gmail.com>
2019-05-02 00:26:59 -0400

    Added Platform enumeration and OsHelper functionality. Upped package to 1.0.2.

---
+++ source/R5T.NetStandard.Types/Code/Enumerations/Platform.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    public enum Platform
+    {
+        Windows,
+        NonWindows,
+    }
+
+
+    public static class PlatformExtensions
+    {
+        public const string WindowsName = @"Windows";
+        public const string NonWindowsName = @"NonWindows";
+
+
+        public static string ToStringStandard(this Platform platform)
+        {
+            switch (platform)
+            {
+                case Platform.NonWindows:
+                    return PlatformExtensions.NonWindowsName;
+
+                case Platform.Windows:
+                    return PlatformExtensions.WindowsName;
+
+                default:
+                    throw new Exception(EnumHelper.GetUnexpectedEnumerationValueMessage(platform));
+            }
+        }
+    }
+}
---
+++ source/R5T.NetStandard.Types/Code/Helpers/OsHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+
+namespace R5T.NetStandard.Types.Code.Helpers
+{
+    public static class OsHelper
+    {
+        public static void PlatformSwitch(Action windows, Action osx, Action linux)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                windows();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                osx();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                linux();
+            }
+            else
+            {
+                throw new Exception(@"Unknown operating system (not Windows, OSX, or Linux).");
+            }
+        }
+
+        public static T PlatformSwitch<T>(Func<T> windows, Func<T> osx, Func<T> linux)
+        {
+            T output = default;
+            OsHelper.PlatformSwitch(
+                () =>
+                {
+                    output = windows();
+                },
+                () =>
+                {
+                    output = osx();
+                },
+                () =>
+                {
+                    output = linux();
+                });
+            return output;
+        }
+
+        public static void DisplayOsName(StreamWriter writer)
+        {
+            OsHelper.PlatformSwitch(
+                () =>
+                {
+                    writer.WriteLine(@"OS: Windows");
+                },
+                () =>
+                {
+                    writer.WriteLine(@"OS: OSX (Mac)");
+                },
+                () =>
+                {
+                    writer.WriteLine(@"OS: Linux");
+                });
+        }
+    }
+}
