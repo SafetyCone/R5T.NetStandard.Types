dc51f3870b67264d585301514b06845df4a8fb07
MinexAutomation <minex.automation@gmail.com>
2019-06-03 18:08:39 -0400

    Added StringHelper with functionality for testing the return value of String.IndexOf() to see if the search string was found.

---
+++ source/R5T.NetStandard.Types/Code/Helpers/StringHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// The return value of the <see cref="String.IndexOf(string)"/> method when the search string is not found.
+        /// </summary>
+        public const int IndexOfNotFound = -1;
+
+
+        /// <summary>
+        /// Useful for testing the return value of the <see cref="String.IndexOf(string)"/> method to see if the search string was found.
+        /// </summary>
+        public static bool IsFound(int index)
+        {
+            var isFound = index != StringHelper.IndexOfNotFound;
+            return isFound;
+        }
+    }
+}
