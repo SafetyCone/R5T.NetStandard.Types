0eb62e035a7bed47821fef9dc6dab0b3cfe2b954
MinexAutomation <minex.automation@gmail.com>
2019-06-04 17:09:00 -0400

    Added VersionHelper class.

---
+++ source/R5T.NetStandard.Types/Code/Helpers/VersionHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    public static class VersionHelper
+    {
+        public static Version None = null;
+
+
+        public static bool IsNone(Version version)
+        {
+            var isNone = version == VersionHelper.None;
+            return isNone;
+        }
+    }
+}
