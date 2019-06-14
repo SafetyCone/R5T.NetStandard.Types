3fd1fec2228b4ef13a1c94ac6a389542544521ae
MinexAutomation <minex.automation@gmail.com>
2019-06-14 19:28:43 -0400

    Moved IConsumer interface code-file, added DoNothingConsumer type.

---
+++ source/R5T.NetStandard.Types/Code/Consumers/Classes/DoNothingConsumer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.NetStandard.Types
+{
+    public class DoNothingConsumer<T> : IConsumer<T>
+    {
+        public static DoNothingConsumer<T> Instance { get; } = new DoNothingConsumer<T>();
+
+
+        public void Consume(T value)
+        {
+            // Do nothing.
+        }
+    }
+}
