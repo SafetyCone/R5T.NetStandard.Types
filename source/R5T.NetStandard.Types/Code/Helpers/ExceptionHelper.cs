1a15704690d2ab8ca036a1c19d738fdd9b8f4297
MinexAutomation <minex.automation@gmail.com>
2019-06-14 14:41:26 -0400

    Added MessageHelper and ExceptionHelper functionality.

---
+++ source/R5T.NetStandard.Types/Code/Helpers/ExceptionHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    public static class ExceptionHelper
+    {
+        /// <summary>
+        /// Provides a message indicating that an instance was not of the expected type.
+        /// </summary>
+        public static string UnexpectedTypeExceptionMessage(Type expectedType, Type foundType)
+        {
+            var output = $@"Wrong type. Expected: {expectedType.FullName}, Found: {foundType.FullName}";
+            return output;
+        }
+
+        /// <summary>
+        /// Provides a message indicating that an instance was not of the expected type.
+        /// </summary>
+        public static string UnexpectedTypeExceptionMessage<TFound>(Type expectedType, TFound foundInstance)
+        {
+            var output = ExceptionHelper.UnexpectedTypeExceptionMessage(expectedType, foundInstance.GetType());
+            return output;
+        }
+
+        /// <summary>
+        /// Provides a message indicating that an instance was not of the expected type.
+        /// </summary>
+        public static string UnexpectedTypeExceptionMessage<TExpected>(object foundInstance)
+        {
+            var output = ExceptionHelper.UnexpectedTypeExceptionMessage(typeof(TExpected), foundInstance.GetType());
+            return output;
+        }
+
+        /// <summary>
+        /// Provides a message indicating that an object could not be cast to an expected type.
+        /// </summary>
+        public static string InvalidCastExceptionMessage(Type expectedType, object obj)
+        {
+            var output = $@"Wrong type. Expected {expectedType}, found: {obj.GetType().FullName}";
+            return output;
+        }
+
+        /// <summary>
+        /// Convenience method that provides a message indicating that an object could not be cast to an expected type.
+        /// </summary>
+        public static string InvalidCastExceptionMessage<TExpected>(object obj)
+        {
+            var expectedType = typeof(TExpected);
+
+            var output = ExceptionHelper.InvalidCastExceptionMessage(expectedType, obj);
+            return output;
+        }
+
+        public static string ArgumentOutOfRangeExceptionMessage(string formattedValue, string formattedMinimum, bool minimumInclusive, string formattedMaximum, bool maximumInclusive)
+        {
+            var allowedRangeClause = MessageHelper.AllowedRange(formattedMinimum, minimumInclusive, formattedMaximum, maximumInclusive);
+
+            var message = MessageHelper.JoinClauses($"Argument out of range: {formattedValue}.", allowedRangeClause);
+            return message;
+        }
+
+        public static string ArgumentOutOfRangeExceptionMessage<TValue>(TValue value, TValue minimum, bool minimumInclusive, TValue maximum, bool maximumInclusive, Func<TValue, string> formatter)
+        {
+            var formattedValue = formatter(value);
+            var formattedMinimum = formatter(minimum);
+            var formattedMaximum = formatter(maximum);
+
+            var message = ExceptionHelper.ArgumentOutOfRangeExceptionMessage(formattedValue, formattedMinimum, minimumInclusive, formattedMaximum, maximumInclusive);
+            return message;
+        }
+
+        public static string ArgumentOutOfRangeExceptionMessage<TValue>(TValue value, TValue minimum, bool minimumInclusive, TValue maximum, bool maximumInclusive)
+        {
+            string DefaultFormatter(TValue x) => x.ToString();
+
+            var message = ExceptionHelper.ArgumentOutOfRangeExceptionMessage(value, minimum, minimumInclusive, maximum, maximumInclusive, DefaultFormatter);
+            return message;
+        }
+
+        public static string ArgumentOutOfRangeExceptionMessage<TValue>(TValue value, TValue minimum, TValue maximum)
+        {
+            var defaultMinimumInclusive = true;
+            var defaultMaximumInclusive = true;
+
+            var message = ExceptionHelper.ArgumentOutOfRangeExceptionMessage(value, minimum, defaultMinimumInclusive, maximum, defaultMaximumInclusive);
+            return message;
+        }
+    }
+}
---
+++ source/R5T.NetStandard.Types/Code/Helpers/MessageHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using R5T.NetStandard.Extensions;
+
+
+namespace R5T.NetStandard
+{
+    public static class MessageHelper
+    {
+        /// <summary>
+        /// One space between clauses (sentences).
+        /// </summary>
+        public const string DefaultClauseSeparator = " ";
+
+
+        public static string AllowedRange(string formattedMinimum, bool minimumInclusive, string formattedMaximum, bool maximumInclusive)
+        {
+            var minimumInclusiveSymbol = minimumInclusive ? '[' : '(';
+            var maximumInclusiveSymbol = maximumInclusive ? ']' : ')';
+
+            var message = $"Allowed range: {minimumInclusiveSymbol}{formattedMinimum}, {formattedMaximum}{maximumInclusiveSymbol}";
+            return message;
+        }
+
+        public static string AllowedRange<TValue>(TValue minimum, bool minimumInclusive, TValue maximum, bool maximumInclusive, Func<TValue, string> formatter)
+        {
+            var formattedMinimum = formatter(minimum);
+            var formattedMaximum = formatter(maximum);
+
+            var message = MessageHelper.AllowedRange(formattedMinimum, minimumInclusive, formattedMaximum, maximumInclusive);
+            return message;
+        }
+
+        public static string AllowedRange<TValue>(TValue minimum, bool minimumInclusive, TValue maximum, bool maximumInclusive)
+        {
+            string DefaultFormatter(TValue x) => x.ToString();
+
+            var message = MessageHelper.AllowedRange(minimum, minimumInclusive, maximum, maximumInclusive, DefaultFormatter);
+            return message;
+        }
+
+        public static string AllowedRange<TValue>(TValue minimum, TValue maximum)
+        {
+            var defaultMinimumInclusive = true;
+            var defaultMaximumInclusive = true;
+
+            var message = MessageHelper.AllowedRange(minimum, defaultMinimumInclusive, maximum, defaultMaximumInclusive);
+            return message;
+        }
+
+        public static string JoinClauses(params string[] clauses)
+        {
+            var message = MessageHelper.JoinClausesSpecifySeparator(MessageHelper.DefaultClauseSeparator, clauses);
+            return message;
+        }
+
+        public static string JoinClausesSpecifySeparator(string clauseSeparator, params string[] clauses)
+        {
+            var message = clauses.Concatenate(MessageHelper.DefaultClauseSeparator);
+            return message;
+        }
+    }
+}
