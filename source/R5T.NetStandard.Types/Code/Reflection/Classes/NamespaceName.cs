8bb2c88edff819efceb8abbf4833e70311009b34
MinexAutomation <minex.automation@gmail.com>
2019-06-14 14:41:53 -0400

    Added reflection-related types.

---
+++ source/R5T.NetStandard.Types/Code/Reflection/Classes/NamespaceName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.NetStandard.Reflection
+{
+    public class NamespaceName : TypedString
+    {
+        public NamespaceName(string value)
+            : base(value)
+        {
+        }
+    }
+}
---
+++ source/R5T.NetStandard.Types/Code/Reflection/Classes/TypeName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.NetStandard.Reflection
+{
+    public class TypeName : TypedString
+    {
+        public TypeName(string value)
+            : base(value)
+        {
+        }
+    }
+}
