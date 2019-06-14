61d0f9704a38d293f0668d1eb651dcb241a20c2f
MinexAutomation <minex.automation@gmail.com>
2019-06-14 16:01:14 -0400

    Removed Get- prefix from EnumHelper methods.

--- source/R5T.NetStandard.Types/Code/Helpers/EnumHelper.cs
+++ source/R5T.NetStandard.Types/Code/Helpers/EnumHelper.cs
@@ -8,7 +8,7 @@ namespace R5T.NetStandard
         /// <summary>
         /// Gets a message indicating that the input string representation of an enumeration value was not recognized among the string representations of a possible values of the <typeparamref name="TEnum"/> enumeration.
         /// </summary>
-        public static string GetUnrecognizedEnumerationValueMessage<TEnum>(string value)
+        public static string UnrecognizedEnumerationValueMessage<TEnum>(string value)
             where TEnum : Enum // Requires C# 7.3.
         {
             var output = $@"Unrecognized enumeration value string '{value}' for enumeration {typeof(TEnum).FullName}";
@@ -19,7 +19,7 @@ namespace R5T.NetStandard
         /// Gets a message indicating that the input string representation of an enumeration value was not recognized among the string representations of a possible values of the <typeparamref name="TEnum"/> enumeration.
         /// Note: This legacy method that restricts <typeparamref name="TEnum"/> as a struct, instead of an <see cref="Enum"/>, is provided since <see cref="Enum.TryParse{TEnum}(string, out TEnum)"/> restricts on struct instead of <see cref="Enum"/>.
         /// </summary>
-        public static string GetUnrecognizedEnumerationValueMessageLegacy<TEnum>(string value)
+        public static string UnrecognizedEnumerationValueMessageLegacy<TEnum>(string value)
             where TEnum : struct // Must be a struct for the moment, since Enum.TryParse<TEnum> requires TEnum to be a struct.
         {
             var output = $@"Unrecognized enumeration value string '{value}' for enumeration {typeof(TEnum).FullName}";
@@ -35,7 +35,7 @@ namespace R5T.NetStandard
         {
             if (!Enum.TryParse(value, out TEnum output))
             {
-                throw new Exception(EnumHelper.GetUnrecognizedEnumerationValueMessageLegacy<TEnum>(value));
+                throw new Exception(EnumHelper.UnrecognizedEnumerationValueMessageLegacy<TEnum>(value));
             }
 
             return output;
@@ -48,7 +48,7 @@ namespace R5T.NetStandard
         /// <remarks>
         /// See: https://stackoverflow.com/questions/13645149/what-is-the-correct-exception-to-throw-for-unhandled-enum-values
         /// </remarks>
-        public static string GetUnexpectedEnumerationValueMessage<TEnum>(TEnum value)
+        public static string UnexpectedEnumerationValueMessage<TEnum>(TEnum value)
             where TEnum : Enum
         {
             var output = $@"Unexpected enumeration value: '{value.ToString()}' for enumeration type {typeof(TEnum).FullName}";
