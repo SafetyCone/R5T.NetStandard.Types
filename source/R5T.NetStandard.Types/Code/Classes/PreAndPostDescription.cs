d69228fed4197a08501d834e7b3150a1302afd4a
MinexAutomation <minex.automation@gmail.com>
2019-06-18 16:22:12 -0400

    Added pre- and post-description functionality for use with logging (and other functionality).

---
+++ source/R5T.NetStandard.Types/Code/Classes/PreAndPostDescription.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    /// <summary>
+    /// Useful for describing before- and after-action results.
+    /// </summary>
+    public class PreAndPostDescription
+    {
+        public PreAndPostVerbPair Verbs { get; set; }
+        public string Description { get; set; }
+
+
+        public PreAndPostDescription()
+        {
+        }
+
+        public PreAndPostDescription(PreAndPostVerbPair verbs, string description)
+        {
+            this.Verbs = verbs;
+            this.Description = description;
+        }
+
+        public PreAndPostDescription(string pre, string post, string description)
+            : this(new PreAndPostVerbPair(pre, post), description)
+        {
+        }
+
+        public string PreDescription()
+        {
+            var output = $"{this.Verbs.Pre} {this.Description}...";
+            return output;
+        }
+
+        public string PostDescription()
+        {
+            var output = $"{this.Verbs.Post} {this.Description}.";
+            return output;
+        }
+    }
+}
---
+++ source/R5T.NetStandard.Types/Code/Classes/PreAndPostVerbPair.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    /// <summary>
+    /// Useful for describing before- and after-action results.
+    /// </summary>
+    public class PreAndPostVerbPair
+    {
+        public string Pre { get; set; }
+        public string Post { get; set; }
+
+
+        public PreAndPostVerbPair()
+        {
+        }
+
+        public PreAndPostVerbPair(string pre, string post)
+        {
+            this.Pre = pre;
+            this.Post = post;
+        }
+    }
+}
