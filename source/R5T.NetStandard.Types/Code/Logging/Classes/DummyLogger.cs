eb3f95ab6d2d7b7bcd97d8e47a16cfc0ddbee104
MinexAutomation <minex.automation@gmail.com>
2019-06-20 10:54:00 -0400

    Moved DummyLogger from R5T.NetStandard to R5T.NetStandard.Types.

---
+++ source/R5T.NetStandard.Types/Code/Logging/Classes/DummyLogger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+
+namespace R5T.NetStandard
+{
+    /// <summary>
+    /// Does nothing. Useful when a logger is required but you may not want to supply a logger.
+    /// </summary>
+    public class DummyLogger : ILogger
+    {
+        public static DummyLogger Instance { get; } = new DummyLogger();
+
+
+        private DummyLogger()
+        {
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            // Do nothing.
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            // Always on.
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            // Do nothing.
+        }
+    }
+}
