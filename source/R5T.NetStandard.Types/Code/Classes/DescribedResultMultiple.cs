f7ce58577a45d1bf0230bc86a65b87ccabb640c8
MinexAutomation <minex.automation@gmail.com>
2019-06-24 13:12:24 -0400

    Added DescribedResultMultiple<T> functionality for describing a result with mulitple messages.

---
+++ source/R5T.NetStandard.Types/Code/Classes/DescribedResultMultiple.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace R5T.NetStandard
+{
+    public static class DescribedResultMultiple
+    {
+        public static readonly IEnumerable<string> DefaultMessages = Enumerable.Empty<string>();
+
+
+        public static DescribedResultMultiple<T> FromValue<T>(T value)
+        {
+            var describedResult = new DescribedResultMultiple<T>(value, DescribedResultMultiple.DefaultMessages);
+            return describedResult;
+        }
+
+        public static DescribedResultMultiple<T> FromValue<T>(T value, string message)
+        {
+            var describedResult = new DescribedResultMultiple<T>(value, message);
+            return describedResult;
+        }
+
+        public static DescribedResultMultiple<T> FromValue<T>(T value, params string[] messages)
+        {
+            var describedResult = new DescribedResultMultiple<T>(value, messages);
+            return describedResult;
+        }
+
+        public static DescribedResultMultiple<T> FromValue<T>(T value, IEnumerable<string> messages)
+        {
+            var describedResult = new DescribedResultMultiple<T>(value, messages);
+            return describedResult;
+        }
+    }
+
+
+    public class DescribedResultMultiple<T>
+    {
+        public T Value { get; }
+        private List<string> Messages_Internal { get; } = new List<string>();
+        public IEnumerable<string> Messages => this.Messages_Internal;
+
+
+        public DescribedResultMultiple(T value, IEnumerable<string> messages)
+        {
+            this.Value = value;
+            this.Messages_Internal.AddRange(messages);
+        }
+
+        public DescribedResultMultiple(T value, params string[] messages)
+            : this(value, messages as IEnumerable<string>)
+        {
+        }
+    }
+}
