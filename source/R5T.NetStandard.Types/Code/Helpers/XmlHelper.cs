586d4b49084cbb67731a4b0b0ef9de08bce31d93
MinexAutomation <minex.automation@gmail.com>
2019-06-26 16:35:04 -0400

    Added XmlHelper functionality.

---
+++ source/R5T.NetStandard.Types/Code/Helpers/XmlHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml;
+
+
+namespace R5T.NetStandard
+{
+    public static class XmlHelper
+    {
+        public const XmlNode SelectSingleNodeNotFound = null;
+
+
+        public static bool SelectSingleNodeFound(XmlNode xmlNode)
+        {
+            var found = xmlNode != XmlHelper.SelectSingleNodeNotFound;
+            return found;
+        }
+    }
+}
