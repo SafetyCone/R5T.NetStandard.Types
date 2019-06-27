aa938641f56868c2eac681f13fee81f7b0bb5017
MinexAutomation <minex.automation@gmail.com>
2019-06-27 09:21:44 -0400

    Added Typed<T> class as base-class for strong-typed has-a T value. Implements IEquatable, but not IComparable.

---
+++ source/R5T.NetStandard.Types/Code/Classes/Typed.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    public class Typed<T> : IEquatable<Typed<T>>
+    {
+        public T Value { get; }
+
+
+        public Typed(T value)
+        {
+            this.Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !obj.GetType().Equals(this.GetType()))
+            {
+                return false;
+            }
+
+            var objAsTyped = obj as Typed<T>;
+
+            var isEqual = this.Equals_Internal(objAsTyped);
+            return isEqual;
+        }
+
+        protected virtual bool Equals_Internal(Typed<T> other)
+        {
+            var isEqual = this.Value.Equals(other.Value);
+            return isEqual;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = this.Value.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            var output = this.Value.ToString();
+            return output;
+        }
+
+        public bool Equals(Typed<T> other)
+        {
+            if (other == null || !other.GetType().Equals(this.GetType()))
+            {
+                return false;
+            }
+
+            var isEqual = this.Equals_Internal(other);
+            return isEqual;
+        }
+    }
+}
