acfc603d20cbd72d3ea2b967536c0189b5ca33c1
MinexAutomation <minex.automation@gmail.com>
2019-07-05 10:46:52 -0400

    Added StringBuilder extensions from Rhodochrosite Common.NetStandard.Lib.

---
+++ source/R5T.NetStandard.Types/Code/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+
+namespace R5T.NetStandard
+{
+    public static class StringBuilderExtensions
+    {
+        public static char LastChar(this StringBuilder builder)
+        {
+            var output = builder[builder.Length - 1];
+            return output;
+        }
+
+        public static string Last(this StringBuilder builder)
+        {
+            var output = builder.LastChar().ToString();
+            return output;
+        }
+
+        public static void RemoveLast(this StringBuilder builder)
+        {
+            builder.Remove(builder.Length - 1, 1);
+        }
+
+        public static void RemoveFromEnd(this StringBuilder builder, string @string)
+        {
+            var stringLength = @string.Length;
+            builder.Remove(builder.Length - stringLength, stringLength);
+        }
+
+        public static StringBuilder EndLineIfNotEnded(this StringBuilder builder, char endOfLine)
+        {
+            if (builder.LastChar() != endOfLine)
+            {
+                builder.Append(endOfLine);
+            }
+
+            return builder;
+        }
+
+        public static StringBuilder EndLineIfNotEnded(this StringBuilder builder, string endOfLine)
+        {
+            if (builder.Length > 0 && builder.Last() != endOfLine)
+            {
+                builder.Append(endOfLine);
+            }
+
+            return builder;
+        }
+
+        public static StringBuilder EndLineIfNotEnded(this StringBuilder builder)
+        {
+            builder.EndLineIfNotEnded(Environment.NewLine);
+
+            return builder;
+        }
+    }
+}
