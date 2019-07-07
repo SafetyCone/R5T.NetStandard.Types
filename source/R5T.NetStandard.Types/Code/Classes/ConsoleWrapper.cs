cd1a842d6b6ceb841f3275f6d80ea3b02e3dd3aa
MinexAutomation <minex.automation@gmail.com>
2019-07-07 13:15:40 -0400

    Added an instantiable wrapper type for the static Console functionality. Extension methods can now be created for the Console!

---
+++ source/R5T.NetStandard.Types/Code/Classes/ConsoleWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    public class ConsoleWrapper
+    {
+        #region Static
+
+        public static ConsoleWrapper New()
+        {
+            var consoleWrapper = new ConsoleWrapper();
+            return consoleWrapper;
+        }
+
+        #endregion
+
+
+        public ConsoleWrapper()
+        {
+        }
+
+        /// <summary>
+        /// The fundamental console write operation.
+        /// </summary>
+        public void WriteLine(string format, params object[] values)
+        {
+            Console.WriteLine(format, values);
+        }
+
+        public void WriteLine(string line)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
---
+++ source/R5T.NetStandard.Types/Code/Extensions/ConsoleWrapperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.NetStandard.Extensions;
+
+
+namespace R5T.NetStandard
+{
+    public static class ConsoleWrapperExtensions
+    {
+        public static void WriteLines(this ConsoleWrapper console, IEnumerable<string> lines)
+        {
+            lines.ForEach(line =>
+            {
+                console.WriteLine(line);
+            });
+        }
+    }
+}
