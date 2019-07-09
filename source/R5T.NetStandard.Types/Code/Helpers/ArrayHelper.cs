f87a1202844f597dff134a3ca23b71b8a0102471
MinexAutomation <minex.automation@gmail.com>
2019-07-09 09:42:04 -0400

    Added static ArrayHelper functionality with a FromSingle() method for creating arrays of a single element when desired.

---
+++ source/R5T.NetStandard.Types/Code/Helpers/ArrayHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.NetStandard
+{
+    public static class ArrayHelper
+    {
+        public static T[] FromSingle<T>(T item)
+        {
+            var output = new T[] { item };
+            return output;
+        }
+    }
+}
