1019abc7d21ff3db73e1c6449dd039471d87eac2
MinexAutomation <minex.automation@gmail.com>
2019-07-25 21:11:30 -0400

    Added ExceptLast() ICollection extension methods, and DirectoryHelper Delete() functionality.

---
+++ source/R5T.NetStandard.Types/Code/Extensions/ICollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.NetStandard.Extensions
+{
+    public static class ICollectionExtensions
+    {
+        public static IEnumerable<T> ExceptLast<T>(this ICollection<T> collection, int count)
+        {
+            var totalNumber = collection.Count;
+
+            var maxCount = totalNumber - count;
+
+            var counter = 0;
+            foreach (var item in collection)
+            {
+                if(counter < maxCount)
+                {
+                    counter++;
+                    yield return item;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public static IEnumerable<T> ExceptLast<T>(this ICollection<T> collection)
+        {
+            var output = collection.ExceptLast(1);
+            return output;
+        }
+    }
+}
---
+++ source/R5T.NetStandard.Types/Code/Helpers/DirectoryHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+
+namespace R5T.NetStandard
+{
+    public static class DirectoryHelper
+    {
+        /// <summary>
+        /// Deletes a directory path.
+        /// The <see cref="System.IO.Directory.Delete(string)"/> method throws a <see cref="System.IO.DirectoryNotFoundException"/> if attempting to delete a non-existent directory. This is annoying.
+        /// All you really want is the directory to not exist, so this method simple takes care of checking if the directory exists.
+        /// Also annoying, you need to specify the recursive option to delete a directory with anything in it. This method take care of that.
+        /// Even more annoying, even after specifying the recursive option, the system method will not delete read-only files.
+        /// </summary>
+        public static void Delete(string directoryPath)
+        {
+            if(Directory.Exists(directoryPath))
+            {
+                DirectoryHelper.DisableReadOnly(directoryPath);
+
+                Directory.Delete(directoryPath, true);
+            }
+        }
+
+        public static void DisableReadOnly(string directoryPath)
+        {
+            var directoryInfo = new DirectoryInfo(directoryPath);
+
+            DirectoryHelper.DisableReadOnly(directoryInfo);
+        }
+
+        /// <summary>
+        /// Remove the read-only attribute from all files.
+        /// </summary>
+        /// <remarks>
+        /// Adapted from: https://stackoverflow.com/questions/1982209/cannot-programatically-delete-svn-working-copy
+        /// </remarks>
+        public static void DisableReadOnly(DirectoryInfo directoryInfo)
+        {
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                if (file.IsReadOnly)
+                {
+                    file.IsReadOnly = false;
+                }
+            }
+
+            foreach (var subdirectory in directoryInfo.GetDirectories())
+            {
+                DirectoryHelper.DisableReadOnly(subdirectory);
+            }
+        }
+    }
+}
