34b32f67b317b181ba9ca35698e082b80260a5cd
MinexAutomation <minex.automation@gmail.com>
2019-09-08 14:44:58 -0400

    Added overridable value type.

---
+++ source/R5T.NetStandard.Types/Code/Classes/OverridableValue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.NetStandard
+{
+    /// <summary>
+    /// Allows for a value to have a default value, but be overridden to have a different value.
+    /// </summary>
+    public class OverridableValue<T>
+    {
+        private readonly object zSynchonization = new object();
+        public T Value { get; private set; }
+        public T DefaultValue { get; private set; }
+        public bool IsOverridden { get; private set; }
+
+
+        public OverridableValue(T defaultValue)
+        {
+            this.DefaultValue = defaultValue;
+            this.Value = this.DefaultValue;
+            this.IsOverridden = false;
+        }
+
+        public void Override(T value)
+        {
+            lock(this.zSynchonization)
+            {
+                this.Value = value;
+                this.IsOverridden = true;
+            }
+        }
+
+        public void Reset()
+        {
+            lock(this.zSynchonization)
+            {
+                this.Value = this.DefaultValue;
+                this.IsOverridden = false;
+            }
+        }
+    }
+}
